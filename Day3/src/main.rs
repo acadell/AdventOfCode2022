e360903536a238f525b4448c855db63cd3283622
aj cadell <white2nerdy@gmail.com>
2022-12-06 16:46:42 -0500

    Day3 part 2 WIP, playing with hash sets

--- Day3/src/main.rs
+++ Day3/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, collections::HashSet};
 
 fn main() {
     let input = fs::read_to_string("input.txt")
@@ -7,42 +7,72 @@ fn main() {
     //define input
     let all_lines = input.split("\n");
 
-    let mut matched_items = String::from("");
-
+    // let mut vec = Vec::new();
+    let mut common_items : HashSet<char> = HashSet::new();
+    let mut badges = "";
     let mut sum = 0;
+    let mut badges_sum = 0;
 
     for line in all_lines {
+        let line_items: HashSet<char>  = line.chars().collect();
+        // vec.push(line);
+        // if vec.len() == 3 {
+        //     let bag1 ::HashSet = vec.pop().unwrap().chars().collect();
+        //     let bag2 = vec.pop().unwrap().chars().collect();
+        //     let bag3 = vec.pop().unwrap().chars().collect();
+            
+            
+            
+            
+        // }
+
+        if common_items.is_empty() {
+            common_items = line_items;
+        }
+        else if common_items.len() == 1 {
+            let a = common_items.drain().next().unwrap();
+            badges_sum += score( a);
+        }
+        
+        
+
 
         let count = line.len() - 1; // len is fine here because the input will only be a-z + A-Z 
         let half = count / 2;
 
-        let pocket1 = &line[..half];
-        let pocket2 = &line[half..];
-
-        let mut matching_item = ' ';
-        for item in pocket1.chars() {
-            
-            if pocket2.contains(item) {
-                matching_item = item;
-            }
-            
-        }
+        let matching_item = find_common_item(line, half);
         let item_score = if !matching_item.is_whitespace() {
             score(matching_item)
         }
         else {0};
 
         sum += item_score;
+
+        println!("Whole pocket: {line}");
+        println!("Number of items in pocket: {count}");
         
-        println!("{line}");
-        println!("{count}");
-        println!("Pocket 1: {pocket1}");
-        println!("Pocket 2: {pocket2}");
         println!("Score: {item_score}\n\n");
     }
-    println!("Total Matched items: {matched_items}");
+
     println!("Score: {sum}");
 }
+
+fn find_common_item(line: &str, half: usize) -> char {
+    let pocket1 = &line[..half];
+    let pocket2 = &line[half..];
+    let mut matching_item = ' ';
+    for item in pocket1.chars() {
+    
+        if pocket2.contains(item) {
+            matching_item = item;
+        }
+    
+    }
+    println!("Pocket 1: {pocket1}");
+    println!("Pocket 2: {pocket2}");
+    matching_item
+}
+
 
 
 fn score(item : char) -> i32
