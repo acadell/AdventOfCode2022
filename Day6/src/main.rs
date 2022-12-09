04f1d13db630902e58c6ce3f5c2a25ab778a1628
aj cadell <white2nerdy@gmail.com>
2022-12-09 14:19:01 -0500

    Day 6 cleanup

--- Day6/src/main.rs
+++ Day6/src/main.rs
@@ -6,20 +6,15 @@ fn main() {
 
     //define input
     let all_lines = input.lines();
-
     let buffer_size = 14;
 
     
     for line in all_lines {
         let mut buffer: Vec<char> = Vec::new();
-
         let characters: Vec<char> = line.chars().collect();
         
         for i in 0..characters.len() {
-            
-            // let mut start_index = i;
-            // let mut end_index = start_index + buffer_size;
-            // let next_index = end_index+1;
+
             let next_char = characters[i];
             
             
@@ -27,40 +22,32 @@ fn main() {
             if buffer.len() >= buffer_size {
 
                 //check if buffer is all unique
-                let mut all_unique = true;
-                let mut current_chars: Vec<char> = Vec::new();
-                for j in 0..buffer_size {
-                    let buffer_char = buffer[j];
-                    
-                    if current_chars.contains(&buffer_char) {
-                        all_unique = false;
-                        break;
-                    }
-                    current_chars.push(buffer_char);
-                }
-                current_chars.clear();
+                let all_unique = is_all_unique_characters(buffer_size, &buffer);
 
                 if all_unique {
                     println!("Unique Buffer: {:?} after {i}", buffer);
                     break;
                 }
 
-                // if !buffer.contains(&next_char) {
-                    
-                //     println!("found it at {i} with {next_char}");
-                //     println!("{:?}", buffer);
-                    
-                // }
                 buffer.pop();
             }
             buffer.insert(0, next_char);
-            
-           
-           
-           
-         
         }
-       
-
     }
+}
+
+fn is_all_unique_characters(buffer_size: usize, buffer: &Vec<char>) -> bool {
+    let mut all_unique = true;
+    let mut current_chars: Vec<char> = Vec::new();
+    for j in 0..buffer_size {
+        let buffer_char = buffer[j];
+    
+        if current_chars.contains(&buffer_char) {
+            all_unique = false;
+            break;
+        }
+        current_chars.push(buffer_char);
+    }
+    current_chars.clear();
+    all_unique
 }
